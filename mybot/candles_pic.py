d7756ba207a1c1bd5156a69f02c2ee8861d2e06a
muzhaleks <aleksandrmuzhykov@gmail.com>
2020-11-29 17:26:35 +0200

    commit

--- mybot/candles_pic.py
+++ mybot/candles_pic.py
@@ -1,30 +1,30 @@
-with open('resources/reality_restart.jpg', 'rb') as reality_restart_pic:
+with open('../resources/reality_restart.jpg', 'rb') as reality_restart_pic:
     REALITY_RESTART_PIC = reality_restart_pic.read()
-with open('resources/wish_set.jpg', 'rb') as wish_set_pic:
+with open('../resources/wish_set.jpg', 'rb') as wish_set_pic:
     WISH_SET_PIC = wish_set_pic.read()
-with open('resources/breakthrough.jpg', 'rb') as breakthrough_candle_pic:
+with open('../resources/breakthrough.jpg', 'rb') as breakthrough_candle_pic:
     BREAKTHROUGH_CANDLE_PIC = breakthrough_candle_pic.read()
-with open('resources/polyn_set.jpg', 'rb') as polyn_set_pic:
+with open('../resources/polyn_set.jpg', 'rb') as polyn_set_pic:
     POLYN_SET_PIC = polyn_set_pic.read()
-with open('resources/clear_set_pic.jpg', 'rb') as clear_set_pic:
+with open('../resources/clear_set_pic.jpg', 'rb') as clear_set_pic:
     CLEAR_SET_PIC = clear_set_pic.read()
-with open('resources/keep_calm.jpg', 'rb') as keep_calm_pic:
+with open('../resources/keep_calm.jpg', 'rb') as keep_calm_pic:
     KEEP_CALM_PIC = keep_calm_pic.read()
-with open('resources/strong_pray.jpg', 'rb') as stronger_pray_candle_pic:
+with open('../resources/strong_pray.jpg', 'rb') as stronger_pray_candle_pic:
     STRONGER_PRAY_CANDLE_PIC = stronger_pray_candle_pic.read()
-with open('resources/light_my_mind.jpg', 'rb') as light_my_mind_set_pic:
+with open('../resources/light_my_mind.jpg', 'rb') as light_my_mind_set_pic:
     LIGHT_MY_MIND_SET_PIC = light_my_mind_set_pic.read()
-with open('resources/love_harmonisation.jpg', 'rb') as love_harmonisation_set_pic:
+with open('../resources/love_harmonisation.jpg', 'rb') as love_harmonisation_set_pic:
     LOVE_HARMONISATION_SET_PIC = love_harmonisation_set_pic.read()
-with open('resources/make_love.jpg', 'rb') as love_make_candle_pic:
+with open('../resources/make_love.jpg', 'rb') as love_make_candle_pic:
     LOVE_MAKE_CANDLE_PIC = love_make_candle_pic.read()
-with open('resources/sweet_couple.jpg', 'rb') as sweet_couple_set_pic:
+with open('../resources/sweet_couple.jpg', 'rb') as sweet_couple_set_pic:
     SWEET_COUPLE_SET_PIC = sweet_couple_set_pic.read()
-with open('resources/give_me_money.jpg', 'rb') as give_me_my_money_candle_pic:
+with open('../resources/give_me_money.jpg', 'rb') as give_me_my_money_candle_pic:
     GIVE_ME_MONEY_CANDLE_PIC = give_me_my_money_candle_pic.read()
-with open('resources/clean_my_money.jpg', 'rb') as clean_my_money_pic:
+with open('../resources/clean_my_money.jpg', 'rb') as clean_my_money_pic:
     CLEAN_MY_MONEY_PIC = clean_my_money_pic.read()
-with open('resources/make_money_set.jpg', 'rb') as financial_success_set_pic:
+with open('../resources/make_money_set.jpg', 'rb') as financial_success_set_pic:
     FINANCIAL_SUCCESS_SET_PIC = financial_success_set_pic.read()
-with open('resources/cleaning_and_filling_set_pic.jpg', 'rb') as cleaning_and_filling_set_pic:
+with open('../resources/cleaning_and_filling_set_pic.jpg', 'rb') as cleaning_and_filling_set_pic:
     CLEANING_AND_FILLING_SET_PIC = cleaning_and_filling_set_pic.read()
--- mybot/utilits.py
+++
@@ -1,9 +0,0 @@
-from telebot import types
-
-
-def generate_keyboard(*answer):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for item in answer:
-        button = types.KeyboardButton(item)
-        keyboard.add(button)
-    return keyboard
